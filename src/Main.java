e31595f23bc079a8e1f3a7184adaf00c74c3525e
Mateus Pereira <maat015z@gmail.com>
2023-06-13 08:25:30 -0300

    Orientação de objetos exemplo eva e adão

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
 public class Main {
-    public Main() {
-    }
 
     public static void main(String[] args) {
-        Pessoa adao = new Pessoa();
+        Pessoa adao;
+         adao = new Pessoa();
+
         Pessoa eva = new Pessoa();
+
         adao.nome = "Adão";
+
         adao.sobrenome = "Silva";
+
         eva.nome = "Eva";
+
         eva.sobrenome = "Silva";
+
         adao.falar();
+
         eva.falar();
-        new Pessoa();
-        Pessoa rainha = new Pessoa();
+
+        Pessoa qualquer = new Pessoa();
+
+        Pessoa rainha;
+
+        rainha = new Pessoa();
         rainha.nome = "Tina";
+
         rainha.sobrenome = "Tunner";
+
         System.out.println(rainha.falar("alto"));
         rainha.comer();
     }
